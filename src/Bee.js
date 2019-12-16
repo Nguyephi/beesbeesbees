f2731b513cd39b68d65c63c3e411cb72b973a349
Phil <pronewgen92@gmail.com>
2019-12-16 15:07:04 -0500

    mini-sprint; beesbeesbees complete.

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,21 @@
 var Bee = function() {
+  // call grub
+  Grub.call(this)
+  this.age = 5;
+  this.color = yellow;
+  this.food =
+  this.job = 'keep on growing'
+};var Bee = function() {
+  // call grub
+  Grub.call(this)
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+
+// set the prototype
+Bee.prototype = Object.create(Grub.prototype);
+
+// set the constructor
+Bee.prototype.constructor = Bee;
 
